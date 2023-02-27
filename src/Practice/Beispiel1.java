b13378d17f8a52a672728188fec860bff50a6931
Esra <esracaglar9067@gmail.com>
2023-02-27 15:01:31 +0100

    Base project files Upload

---
+++ src/Practice/Beispiel1.java
@@ -0,0 +1,7 @@
+package Practice;
+
+public class Beispiel1 {
+    public static void main(String[] args) {
+        System.out.println("Beispiel1" );
+    }
+}
---
+++ src/Practice/Beispiel2.java
@@ -0,0 +1,7 @@
+package Practice;
+
+public class Beispiel2 {
+    public static void main(String[] args) {
+        System.out.println("Beispiel 2");
+    }
+}
